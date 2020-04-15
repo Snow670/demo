64644b57e196c8b78d1c07c0a1c0b6c358614add
Dell <1419517126@qq.com>
2020-04-15 10:16:50 +0800

    vue实战布局&首页

--- simple/src/main.js
+++ simple/src/main.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Base from './Base.vue'
+import VueRouter from 'vue-router'
+import IndexPage from './pages/index.vue'
+
+
+Vue.use(VueRouter)
+
+let router = new VueRouter({
+  mode:'history',
+  routes:[
+    {
+      path:'/',
+      component:IndexPage
+    }
+  ]
+})
 
 new Vue({
   el: '#app',
+  router,
   components:{
-    App
-  }
-
+    Base
+  },
+  template:'<Base/>'
 })
